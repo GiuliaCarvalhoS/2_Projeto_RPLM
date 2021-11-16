cfea9c77e491ef33394d78322fa2cfd80b0c78e9
Fernando Dias <90704932+FernandoDias18@users.noreply.github.com>
2021-11-16 11:58:42 -0300

    Faltando as regras somente

--- trabalho.py
+++ trabalho.py
@@ -49,10 +49,12 @@ mixer.music.stop()
 
 # Terceiro parágrafo
 print("")
-print("Parágrafo 3")
-print("Vocês saem da cozinha em direção ao grito, que parece vir da sala de estar. Ao chegar lá, você se depara com o corpo estendido do zé do caixão no chão.\n\033[31mHá um corte no corpo do Zé, indo da sua garganta até um pouco abaixo do umbigo… \033[0m")
-
-img = Image.open('AREA9 (1).png')  # cria o objeto
+print("Vocês saem da cozinha em direção ao grito, que parece vir da sala de estar. Ao chegar lá, você se depara com o "
+      "\033[31mcorpo estendido de Zé do Caixão no chão.\nHá um corte no corpo do Zé, indo da sua garganta até um pouco"
+      " abaixo do umbigo… \033[0m")
+time.sleep(10)
+#imagem local do assassinato
+img = Image.open('./imagens/sala_1.jpg')  # cria o objeto
 
 img.show() #apresenta a imagem na tela
 print("-" * 180)
@@ -68,7 +70,7 @@ preposicoesT = ['Se você, Jigsaw e Chucky estavam na cozinha, então o assassin
                 'Se a faca de PennyWise era de churrasco, então ele estava cortando as pernas das crianças para o churras',
                 'Se Anabelle está mexendo no anel sobre a luva, então zé morreu esfaqueado'
                 ]
-
+pistasescolhidas = ["Zé do caixão morreu esfaqueado", "Para ir a varanda é necessário passar pelo quintal frontal"]
 menu(preposicoes)
 
 print("-" * 180)
@@ -83,6 +85,7 @@ while primeiraEscolha < 0 or primeiraEscolha > len(preposicoes):
             print("-" * 180)
             print(f'\033[1;34m{preposicoesT[primeiraEscolha - 1]}\033[0m')
             print("-" * 180)
+            pistasescolhidas.append(preposicoesT[primeiraEscolha - 1])
             preposicoesT.pop(primeiraEscolha - 1)
             break
     except ValueError:
@@ -100,6 +103,7 @@ while segundaEscolha < 0 or segundaEscolha > len(preposicoes):
             print("-" * 180)
             print(f'\033[1;34m{preposicoesT[segundaEscolha - 1]}\033[0m')
             print("-" * 180)
+            pistasescolhidas.append(preposicoesT[segundaEscolha - 1])
             preposicoesT.pop(segundaEscolha - 1)
             break
     except ValueError:
@@ -122,8 +126,11 @@ if escolhassassino == "S":
         mixer.music.play()
         time.sleep(2.5)
         mixer.music.stop()
+        # imagem jogador morreu
+        img = Image.open('./imagens/VOCE_MORREU.jpg')  # cria o objeto
+        img.show()  # apresenta a imagem na tela
         print("-" * 180)
-        print("\033[31mVOCÊ MORREU\033[0m")
+        print("\033[31mVOCÊ MORREU\033[0m".center(180))
         print("-" * 180)
         sys.exit()
     else:
@@ -158,8 +165,10 @@ else:
           "estavam na sala - diz Chucky\n"
           "- Vamos jogar um jogo…\n"
           "-Ah cala a boca !!! - Você e Chucky falam ao mesmo tempo\n"
-          "- Deixem aquela boneca comigo! diz Chucky\n"
-          "- Você parece muito interessado na Anabelle, Chucky, questiona Jigsaw\n" #mudar isso, deixar apenas piada do carro
+          "- Deixem aquela boneca comigo! diz Chucky")
+    print("-" * 180)
+    time.sleep(10)
+    print("- Você parece muito interessado na Anabelle, Chucky, questiona Jigsaw\n" 
           "- Ela tem que respeitar meus 32 anos de carreira. Ela tá começando a vida dela.\nEu não comecei ontem, tenho toda uma história de vida,"
           " tenho toda uma história de trabalho. Eu fui pioneiro do movimento, hoje eu sou um ícone, vocifera Chucky\n"
           "- Então vamos ficar de olho em cada um? Pergunta Jigsaw\n"
@@ -175,17 +184,24 @@ else:
     mixer.music.stop()
     print("-" * 180)
     print("Você escuta um grito, dessa vez vindo da garagem")
-    print("Vocês correm direto para o som do grito\n"
-          "\033[31mLá encontram Samara\033[0m….\nuuuuu… que interessante.. Agora restam dois suspeitos...ou será que não ?!\n"
+    # imagem local do assassinato
+    img = Image.open('./imagens/garagem.jpg') # cria o objeto
+
+    img.show()  # apresenta a imagem na tela
+    print("Você corre direto para o som do grito\n"
+          "\033[31mLá você encontra Samara\033[0m….\nuuuuu… que interessante.. Agora restam dois suspeitos...ou será que não ?!\n"
           "Vamos falar então da morte da Samara?!\n"
           "Ela foi encontrada no carro, com os cabelos em pé"
-          "- A garota tentou fazer uma ligação direta no carro e foi ligada com Deus hahah - Chucky caçoa\n"
-          "- Não seja idiota! Mexa nela… viu, assassinada! - Comenta Jigsaw, de sua bicicletinha. Como a bicicleta de Jigsaw é"
-          "muito baixa para\n"
-          "conseguir ver pela janela, abriram a porta do carro para Jigsaw ver melhor"
-          "Samara estava parecida com Nick-quase-sem-cabeça (se você não entendeu a referência, sinto muito\n"
-          "-Hum, a mesma arma talvez, o corte está limpo...provavelmente fora o mesmo que matou aquele cara lá, pontua chucky")
-    time.sleep(10)
+          "- A garota tentou fazer uma ligação direta no carro e foi ligada com Deus hahah - caçoa Annabele\n"
+          "Aparentemente você não foi o primeiro a chegar no local do crime... Talvez o grito tenha vindo de Anabelle ?!\n ")
+    print("-" * 180)
+    time.sleep(15)
+    print("Olha, estamos tentando lhe ajudar. Não que nos importamos com você, mas temos um relacionamento bem extreito com nosso dinheiro\n"
+          "Você consegue ver? Ali! Há um corte no pescoço dela, muito parecido com o que foi feito naquele velho lá do começo\n"
+          "Olha, você está em uma poolparty com assassinos, só segue o roteiro tá legal?! não tente entender como minha voz foi parar na sua cabeça\n"
+          "Enfim... Samara estava parecida com Nick-quase-sem-cabeça (se você não entendeu a referência, sinto muito\n"
+          "-Hum, a mesma arma talvez, o corte está limpo...provavelmente fora o mesmo que matou aquele cara lá, pontua Anabelle")
+    time.sleep(15)
     #Adicionando novas preposições
     preposicoesT.append("Se Annabelle está mexendo no anel sobre a luva, então ela é a assassina ")
     preposicoesT.append("Se voz do grito era feminina, então Samara era quem estava gritando de dor ou a Annabelle de susto")
@@ -207,6 +223,7 @@ else:
                 print("-" * 180)
                 print(f'\033[1;34m{preposicoesT[primeiraEscolha - 1]}\033[0m')
                 print("-" * 180)
+                pistasescolhidas.append(preposicoesT[primeiraEscolha - 1])
                 preposicoesT.pop(primeiraEscolha - 1)
                 break
         except ValueError:
@@ -224,6 +241,7 @@ else:
                 print("-" * 180)
                 print(f'\033[1;34m{preposicoesT[segundaEscolha - 1]}\033[0m')
                 print("-" * 180)
+                pistasescolhidas.append(preposicoesT[segundaEscolha - 1])
                 preposicoesT.pop(segundaEscolha - 1)
                 break
         except ValueError:
@@ -246,8 +264,11 @@ else:
             mixer.music.play()
             time.sleep(2.5)
             mixer.music.stop()
+            # imagem jogador morreu
+            img = Image.open('./imagens/VOCE_MORREU.jpg')  # cria o objeto
+            img.show()  # apresenta a imagem na tela
             print("-" * 180)
-            print("\033[31mVOCÊ MORREU\033[0m")
+            print("\033[31mVOCÊ MORREU\033[0m".center(180))
             print("-" * 180)
             sys.exit()
         else:
@@ -262,12 +283,8 @@ else:
         preposicoes = ["Pista1", "Pista2", "Pista3", "Pista4"]
         print("-" * 180)
         print("Escolha 2 pistas (preposições) :")
-        # Mostra o som do grito
-        mixer.music.load("./sound/y2meta.com - Grito de terror (320 kbps).mp3")
-        mixer.music.play()
-        time.sleep(2.5)
-        mixer.music.stop()
         menu(preposicoes)
+
 
         print("-" * 180)
 
@@ -281,6 +298,7 @@ else:
                     print("-" * 180)
                     print(f'\033[1;34m{preposicoesT[primeiraEscolha - 1]}\033[0m')
                     print("-" * 180)
+                    pistasescolhidas.append(preposicoesT[primeiraEscolha - 1])
                     preposicoesT.pop(primeiraEscolha - 1)
                     break
             except ValueError:
@@ -298,6 +316,7 @@ else:
                     print("-" * 180)
                     print(f'\033[1;34m{preposicoesT[segundaEscolha - 1]}\033[0m')
                     print("-" * 180)
+                    pistasescolhidas.append(preposicoesT[segundaEscolha- 1])
                     preposicoesT.pop(segundaEscolha - 1)
                     break
             except ValueError:
@@ -320,8 +339,11 @@ else:
                 mixer.music.play()
                 time.sleep(2.5)
                 mixer.music.stop()
+                # imagem jogador morreu
+                img = Image.open('./imagens/VOCE_MORREU.jpg')  # cria o objeto
+                img.show()  # apresenta a imagem na tela
                 print("-" * 180)
-                print("\033[31mVOCÊ MORREU\033[0m")
+                print("\033[31mVOCÊ MORREU\033[0m".center(180))
                 print("-" * 180)
                 sys.exit()
             else:
@@ -332,4 +354,52 @@ else:
                 mixer.music.play()
                 time.sleep(3)
         else:
-            print("Historia final")
+            print("-" * 180)
+            print("Você está andando pela casa, pensando no que poderia ter acontecido...")
+            print("As informoções que você sabe até agora:")
+            print(f'1: {pistasescolhidas[0]}')
+            time.sleep(2.5)
+            print(f'2: {pistasescolhidas[1]}')
+            time.sleep(2.5)
+            print(f'3: {pistasescolhidas[2]}')
+            time.sleep(2.5)
+            print(f'4: {pistasescolhidas[3]}')
+            time.sleep(2.5)
+            print(f'5: {pistasescolhidas[4]}')
+            time.sleep(2.5)
+            print(f'6: {pistasescolhidas[5]}')
+            time.sleep(2.5)
+            print(f'7: {pistasescolhidas[6]}')
+            time.sleep(2.5)
+            print(f'8: {pistasescolhidas[7]}')
+            time.sleep(7)
+            print("-" * 180)
+            print('Você ouve um barulho vindo da cozinha.. Ao adentrar a cozinha, o barulho fica mais alto. Parece o som de alguem sendo estrangulado.\n'
+                  'Você decide ir a fundo nisso, entender  o que esta acontecendo…')
+            # Mostra o som do coração
+            mixer.music.load("./sound/Som de coração batendo_48k.mp3")
+            mixer.music.play()
+            time.sleep(3)
+            mixer.music.stop()
+            print("Chegando perto da churrasqueira, o som aumenta. ")
+            # imagem local do assassinato
+            img = Image.open('./imagens/churrasqueira.jpg')  # cria o objeto
+            img.show()  # apresenta a imagem na tela
+            time.sleep(5)
+            print("Então um grito\n"
+                  "E lá está, Anabelle, com a cabeça de Jigsaw em suas mãos.\n"
+                  "Ela arrancou a cabeça dele e.. nossa… parece que está comendo as vísceras\n"
+                  "ELA VEM ATRÁS DE VOCÊ")
+            # Mostra o som derrota
+            mixer.music.load("./sound/mario fail sound effect_48k.mp3")
+            mixer.music.play()
+            time.sleep(5)
+            mixer.music.stop()
+            #Imagem você morreu
+            img = Image.open('./imagens/VOCE_MORREU.jpg')  # cria o objeto
+            img.show()  # apresenta a imagem na tela
+            print("-" * 180)
+            print("\033[31mVOCÊ MORREU\033[0m".center(180))
+            print("-" * 180)
+            sys.exit()
+
